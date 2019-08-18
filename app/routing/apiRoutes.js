42df05bf6834a3584a61b37adb65b3c3cbc032d7
Ben Cooley <bcooley58@gmail.com>
2019-08-17 22:59:09 -0400

    npm init and some work trying to tighten up the survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,33 +7,33 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    const match = {
+    var match = {
       name: "",
       photo: "",
       fDifference: 1000
     };
 
-    const userData = req.body;
-    const userScores = userData.scores;
-    const absoluteDiff = 0;
+    var userData = req.body;
+    var userScores = userData.scores;
+    var absoluteDiff = 0;
 
-    for (var i = 0; i < friendsData.length; i++) {
+    for (var i = 0; i < friends.length; i++) {
       absoluteDiff = 0;
 
       for (var j = 0; j < userScores.length; j++) {
         absoluteDiff += Math.abs(
-          parseInt(friendsData[i].scores[j]) - parseInt(userScores[j])
+          parseInt(friends[i].scores[j]) - parseInt(userScores[j])
         );
       }
 
       if (absoluteDiff <= match.fDifference) {
         match.fDifference = absoluteDiff;
-        match.name = friendsData[i].name;
-        match.photo = friendsData[i].photo;
+        match.name = friends[i].name;
+        match.photo = friends[i].photo;
       }
     }
 
-    friendsData.push(userData);
+    friends.push(userData);
 
     res.json(match);
   });
